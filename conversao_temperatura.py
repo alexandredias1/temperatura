c2abab246f57ea599372461253f9b6fe746106ea
Caualu <caualuizsilvacorrea@gmail.com>
2024-05-29 08:09:10 -0300

    conversao_fahrenheint
    convertendo de celsius e kelvin para fahrenheint e vice versa.

--- conversao_temperatura.py
+++ conversao_temperatura.py
@@ -5,7 +5,7 @@ try:
     while True :
         temp = float(input("Diga a temperatura atual: "))
         #Para o usuário escolher uma opção
-        conversao = int(input("Converter a temperatura para\n1 - Celsius\n2 - Kelvin\n3 - Fahreheint :  "))
+        conversao = int(input("Converter a temperatura para\n1 - Celsius\n2 - Kelvin\n3 - Fahreheint\n4 - Celsius_para_Fahreheint\n5 - Kelvin_para_Fahreheint\n6 - Fahreheint_para_Kelvin\n7 - Sair: "))
 
         temp = float(temp)
 
@@ -19,11 +19,21 @@ try:
             print(f"A temperatura convertida para Celsius ficaria de {Kelvin}°C")
         elif conversao == 3:
             Fahreheint = (temp * 1.8 + 32)
-            print(f"A temperatura convertida para Fahreheint ficaria de {Fahreheint}°F")
+            print(f"A temperatura convertida para Celsius ficaria de {Fahreheint}°C")
         elif conversao == 4:
+            celsius = (temp - 32 / 1.8)
+            print(f"A temperatura convertida para Fahreheint ficaria de {celsius}°F")
+        elif conversao == 5:
+            Kelvin = (temp - 273.15) * 1.8 + 32
+            print(f"A temperatura convertida para Fahreheint ficaria de {Kelvin}°F") 
+        elif conversao == 6:
+            Fahreheint = (temp - 32) / 1.8 + 273.15
+            print(f"A temperatura convertida para kelvin ficaria de {Fahreheint}°K") 
+
+        elif conversao == 7:
             break
         else:
-            print("Valor inserido inválido")
+            print("Numero inserido inválido")
 
 except ValueError:
     print("Valor inserido inválido")
